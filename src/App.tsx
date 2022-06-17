ada267e90c4858457b182fd4821e6acfc20c5a69
yoojeonglee-ge <Yoojeong.Lee@geappliances.com>
2022-06-17 10:55:47 +0900

    Add distortion text analysis API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { overgeneralization, perfectionism, fortuneTelling } from './api/distortionAPI';
 import './App.css';
 
 function App() {
+  const distortionList = ['overgeneralization', 'perfectionism', 'fortune telling'];
+  const statement = (distortion: string, isTrue: boolean): string => isTrue ? `You may have the cognitive distortion ${distortion}.` : '';
+
+  const [log, setLog] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    setResult( statement(distortionList[0], overgeneralization(log)) );
+    event.preventDefault();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => setLog(event.target.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={log} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {result}
     </div>
   );
 }
